8b994f39f318eeb0f31edcb67386af335542478d
Wilmer Arambula <terabytesoftw@gmail.com>
2026-01-06 20:12:57 -0300

    feat(ecs): Add `ECS` configuration file and update workflow to download config.

---
+++ ecs.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ClassNotation\{ClassDefinitionFixer, OrderedClassElementsFixer, OrderedTraitsFixer};
+use PhpCsFixer\Fixer\Import\{NoUnusedImportsFixer, OrderedImportsFixer};
+use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withConfiguredRule(
+        ClassDefinitionFixer::class,
+        [
+            'space_before_parenthesis' => true,
+        ],
+    )
+    ->withConfiguredRule(
+        OrderedClassElementsFixer::class,
+        [
+            'order' => [
+                'use_trait',
+                'constant_public',
+                'constant_protected',
+                'constant_private',
+                'case',
+                'property_public',
+                'property_protected',
+                'property_private',
+                'construct',
+                'destruct',
+                'magic',
+                'phpunit',
+                'method_public',
+                'method_protected',
+                'method_private',
+            ],
+            'sort_algorithm' => 'alpha',
+        ],
+    )
+    ->withConfiguredRule(
+        OrderedImportsFixer::class,
+        [
+            'imports_order' => ['class', 'function', 'const'],
+            'sort_algorithm' => 'alpha',
+        ],
+    )
+    ->withFileExtensions(['php'])
+    ->withPaths(
+        [
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    )
+    ->withPhpCsFixerSets(perCS20: true)
+    ->withPreparedSets(
+        cleanCode: true,
+        comments: true,
+        docblocks: true,
+        namespaces: true,
+        strict: true,
+    )
+    ->withRules(
+        [
+            NoUnusedImportsFixer::class,
+            OrderedTraitsFixer::class,
+            SingleQuoteFixer::class,
+        ]
+    );
