e24401dde1788f7e85f17a50c330f0edbc3bda89
Wilmer Arambula <terabytesoftw@gmail.com>
2026-01-07 09:12:49 -0300

    Fix error typo.

--- ecs.php
+++ ecs.php
@@ -30,6 +30,7 @@ return ECSConfig::configure()
                 'construct',
                 'destruct',
                 'magic',
+                'phpunit',
                 'method_protected_abstract',
                 'method_public',
                 'method_protected',
